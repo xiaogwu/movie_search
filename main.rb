dc7da5811f669c3fb56d7acd9bd80dc3896b02c0
Xiao G. Wu <xiaogwu@gmail.com>
2013-06-26 22:19:07 -0700

    Added tomatoes rating and logic to check for no results found condition

--- main.rb
+++ main.rb
@@ -15,7 +15,7 @@ get '/search' do
 end
 
 get '/movie' do
-  url = "http://www.omdbapi.com/?i=#{URI.escape(params[:id])}"
+  url = "http://www.omdbapi.com/?i=#{URI.escape(params[:id])}&tomatoes=true"
   @movie = JSON.load(open(url).read)
   erb :movie
 end
